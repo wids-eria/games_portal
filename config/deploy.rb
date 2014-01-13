de0bfb62e5971baf3d3dca8a0ac947b9bb4f503b
Isaac Goodin <isaacgoodin@gmail.com>
2014-01-13 11:00:15 -0600

    Changed deploy script to correctly symlink file uploads

--- config/deploy.rb
+++ config/deploy.rb
@@ -63,3 +63,42 @@ namespace :deploy do
     run "cd #{current_path} && touch tmp/restart.txt"
   end
 end
+
+
+# ==============================
+# Paperclip Uploads proper capistrano symlink
+# ==============================
+
+namespace :uploads do
+
+  desc <<-EOD
+    Creates the upload folders unless they exist
+    and sets the proper upload permissions.
+  EOD
+  task :setup, :except => { :no_release => true } do
+    dirs = uploads_dirs.map { |d| File.join(shared_path, d) }
+    run "#{try_sudo} mkdir -p #{dirs.join(' ')} && #{try_sudo} chmod g+w #{dirs.join(' ')}"
+  end
+
+  desc <<-EOD
+    [internal] Creates the symlink to uploads shared folder
+    for the most recently deployed version.
+  EOD
+  task :symlink, :except => { :no_release => true } do
+    run "rm -rf #{release_path}/public/system"
+    run "ln -nfs #{shared_path}/uploads #{release_path}/public/system"
+  end
+
+  desc <<-EOD
+    [internal] Computes uploads directory paths
+    and registers them in Capistrano environment.
+  EOD
+  task :register_dirs do
+    set :uploads_dirs,    %w(uploads uploads/partners)
+    set :shared_children, fetch(:shared_children) + fetch(:uploads_dirs)
+  end
+
+  after       "deploy:finalize_update", "uploads:symlink"
+  on :start,  "uploads:register_dirs"
+
+end
