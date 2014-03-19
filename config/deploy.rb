96085324cd79c660acc29cc181aab8e80a12b3b0
Isaac Goodin <isaacgoodin@gmail.com>
2014-03-19 14:49:24 -0500

    Testing capistrano symlink

--- config/deploy.rb
+++ config/deploy.rb
@@ -69,39 +69,38 @@ end
 # Paperclip Uploads proper capistrano symlink
 # ==============================
 
-namespace :uploads do
+=begin
+  namespace :uploads do
 
-  desc <<-EOD
-    Creates the upload folders unless they exist
-    and sets the proper upload permissions.
-  EOD
-  task :setup, :except => { :no_release => true } do
-    dirs = uploads_dirs.map { |d| File.join(shared_path, d) }
-    run "#{try_sudo} mkdir -p #{dirs.join(' ')} && #{try_sudo} chmod g+w #{dirs.join(' ')}"
+    desc <<-EOD
+      Creates the upload folders unless they exist
+      and sets the proper upload permissions.
+    EOD
+    task :setup, :except => { :no_release => true } do
+      dirs = uploads_dirs.map { |d| File.join(shared_path, d) }
+      run "#{try_sudo} mkdir -p #{dirs.join(' ')} && #{try_sudo} chmod g+w #{dirs.join(' ')}"
+    end
+
+    desc <<-EOD
+      [internal] Creates the symlink to uploads shared folder
+      for the most recently deployed version.
+    EOD
+    task :symlink, :except => { :no_release => true } do
+      run "rm -rf #{release_path}/public/system"
+      run "ln -nfs #{shared_path}/uploads #{release_path}/public/system"
+    end
+
+    desc <<-EOD
+      [internal] Computes uploads directory paths
+      and registers them in Capistrano environment.
+    EOD
+    task :register_dirs do
+      set :uploads_dirs,    %w(uploads uploads/partners)
+      set :shared_children, fetch(:shared_children) + fetch(:uploads_dirs)
+    end
+
+    after       "deploy:finalize_update", "uploads:symlink"
+    on :start,  "uploads:register_dirs"
+
   end
-
-  desc <<-EOD
-    [internal] Creates the symlink to uploads shared folder
-    for the most recently deployed version.
-  EOD
-  task :symlink, :except => { :no_release => true } do
-    run "rm -rf #{release_path}/public/uploads"
-    run "ln -nfs #{shared_path}/#{stage}/uploads #{release_path}/public/uploads"
-  end
-
-  desc <<-EOD
-    [internal] Computes uploads directory paths
-    and registers them in Capistrano environment.
-
-    Note. I can't set value for directories directly in the code because
-    I don't know in advance selected stage.
-  EOD
-  task :register_dirs do
-    set :uploads_dirs,    %w(uploads uploads/partners).map { |d| "#{stage}/#{d}" }
-    set :shared_children, fetch(:shared_children) + fetch(:uploads_dirs)
-  end
-
-  after       "deploy:finalize_update", "uploads:symlink"
-  on :start,  "uploads:register_dirs",  :except => stages + ['multistage:prepare']
-
-end
+=end
