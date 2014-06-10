82f6d97ff3fd71530f885358edcc8d31833b6153
Isaac Goodin <isaacgoodin@gmail.com>
2014-06-10 14:06:30 -0500

    reconfig

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,7 +5,7 @@ GlsPortal::Application.configure do
   config.cache_classes = true
 
   # Full error reports are disabled and caching is turned on
-  config.consider_all_requests_local       = true
+  config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
@@ -15,7 +15,7 @@ GlsPortal::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  #config.assets.compile = false
+  config.assets.compile = false
 
   # Generate digests for assets URLs
   config.assets.digest = true
