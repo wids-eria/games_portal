09232b2f1cf11d0a96ed75cf9c04dcf5b19a75a4
Isaac Goodin <isaacgoodin@gmail.com>
2014-06-10 14:16:01 -0500

    removed fix

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :ada, ENV['ADAName'], ENV['ADASecret'], :callback_path => "http://gamesportal.gameslearningsociety.org/auth/ada/callback",scope:"portal"
+  provider :ada, ENV['ADAName'], ENV['ADASecret'], :callback_path => "/auth/ada/callback",scope:"portal"
 end
