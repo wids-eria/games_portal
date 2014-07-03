9d065e7acaa57d6fc6dd00834f2dcfc099b81a4e
Isaac Goodin <isaacgoodin@gmail.com>
2014-07-03 16:20:56 -0500

    Added SITE_URL variable to application yml to override the default callback url

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :ada, ENV['ADAName'], ENV['ADASecret'], :callback_path => "/auth/ada/callback",scope:"portal"
-OmniAuth.config.full_host = "http://gamesportal.gameslearningsociety.org/"
+  if ENV['SITE_URL']
+    OmniAuth.config.full_host = ENV['SITE_URL']
+  end
 end
