bfc18fadf12bba65cf4d118586a6db33bc8408e5
Isaac Goodin <isaacgoodin@gmail.com>
2014-12-11 14:35:39 -0600

    fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,7 +60,7 @@ GlsPortal::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
-  config.action_mailer.deliver_method = :test
+  config.action_mailer.delivery_method = :test
 
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
